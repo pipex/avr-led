9546bd55acba5701b8c4f50dbf05180f0a47c8c0
Felipe Lalanne <felipe@lalanne.cl>
2019-12-16 17:47:31 -0300

    Add comments to usbreset

--- usbreset.sh
+++ usbreset.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Reset usb devices without rebooting
+# Source: http://billauer.co.il/blog/2013/02/usb-reset-ehci-uhci-linux/
+
 if [[ $EUID != 0 ]] ; then
   echo This must be run as root!
   exit 1
